4098f3c77b1c2ddc085d7001116147c968f01885
Kyle Mayers <kyle7150@gmail.com>
2018-01-22 17:11:10 +0000

    Mongo 1

---
+++ mongo.js
@@ -0,0 +1,6 @@
+1.
+2. db.monsters.insert({})
+3. db.monsters.find()
+4. db.monsters.find({"Name": "Kai"})
+5. db.monsters.find({"Attacks": "Fire"}, {"multi": true})
+6. db.monsters.find({"Stats.Defense": 35})
